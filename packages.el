ec95c9a5a4a441b6469e73252894d7d1bc5fd7e7
Byungsoo Seo <pa0k.su@gmail.com>
2017-04-23 02:22:19 +0900

    fix load function error: korean//set-cjk-fonts
    Tested-by: Byungsoo Seo <pa0k.su@gmail.com>

--- packages.el
+++ packages.el
@@ -52,15 +52,6 @@
         ))
 
 ;;;; Configrations
-;;;;; fonts
-(spacemacs|do-after-display-system-init
- ;; Source Code Pro 보다 DejaVu Sans Mono가 더 익숙해서 바꾸고 보니,
- ;; 일부 알파벳이 네모로 나온다. 대표적으로 볼 일 없는 첫 화면과 info 문서가 그렇다.
- ;; 이 문제는 아래 두 줄이면 해결된다.
- (set-face-attribute 'variable-pitch nil
-                     :family (car dotspacemacs-default-font))
-
- (korean//set-cjk-fonts korean-default-fonts))
 
 ;;;;; input method
 (defun korean/init-korea-util ()
